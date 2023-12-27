8a9415894e79b638951321ccdf1cc6496ef68b92
Alane-Kerolaine <alanekerolaine@live.com>
2023-12-27 14:35:33 -0300

    adicionando atributos a EmprestimoModel

--- src/main/java/com/sistema/bibliotecaonline/model/EmprestimoModel.java
+++ src/main/java/com/sistema/bibliotecaonline/model/EmprestimoModel.java
@@ -1,11 +1,9 @@
 package com.sistema.bibliotecaonline.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Data
@@ -17,4 +15,8 @@ public class EmprestimoModel {
 
     private Date dataEmprestimo;
     private Date dataDevolucao;
+    @OneToMany
+    private List<ExemplarModel> exemplar;
+    @OneToOne
+    private ClienteModel cliente;
 }
